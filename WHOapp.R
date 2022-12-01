0bb1fc785d9863bb5fca26eb53a966e2c485c92b
dnicholls45 <115480786+dnicholls45@users.noreply.github.com>
2022-12-01 08:55:19 +0000

    Add files via upload

---
+++ WHOapp.R
@@ -0,0 +1,51 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(lubridate)
+library(here)
+library(spsComps)
+
+body <- dashboardBody(
+  fluidRow(
+    box(title = "WHO", width = 4, solidHeader = TRUE, 
+        fileInput(inputId = "inputfile", "", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")) %>% 
+          bsPopover("click browse to upload .csv file", "then click download button"),
+        p(),
+        p(),
+        downloadButton("report"), 
+      
+     )
+    )
+  )
+            
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Trial converter"),
+  dashboardSidebar(disable = TRUE),
+  body
+)
+
+
+server <- function(input, output) {
+  
+  output$report <- downloadHandler(
+    
+    filename = "report.doc",
+    content = function(file) {
+      tempReport <- file.path(tempdir(), "who.Rmd")
+      file.copy("who.Rmd", tempReport, overwrite = TRUE)
+      params <- list(report.data = input$inputfile$datapath)
+      rendered_report <- rmarkdown::render(
+        tempReport,
+        output_file = file,
+        params = params,
+        envir = new.env(parent = globalenv())
+      )
+      
+      file.copy(rendered_report, file)
+    }
+  )
+}
+
+shinyApp(ui, server)
+
